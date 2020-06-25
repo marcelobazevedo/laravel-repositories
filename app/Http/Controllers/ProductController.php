829e9aa240487f646dd5898ccc3e180ef56515f9
marcelobazevedo <marcelobazevedo@gmail.com>
2020-06-25 15:56:05 -0300

    21 - Middlewares em Controllers no Laravel

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -6,36 +6,88 @@ use Illuminate\Http\Request;
 
 class ProductController extends Controller
 {
+
+    protected $request;
+    public function __construct(Request $request)
+    {
+        $this->request = $request;
+        //$this->middleware('auth');
+        //$this->middleware('auth')->only(['create', 'store']);
+        $this->middleware('auth')->except('index');
+    }
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
     public function index()
     {
-        $products = ['Product 1', 'Product 2', 'Product 3', 'Product 4'];
+        return 'aqui é o index';
+    }
 
-        return $products;
-    }
-    public function show($id)
-    {
-        return "Exibindo o produto de id: {$id}";
-    }
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
     public function create()
     {
-        return "formulario de produtos";
+        //
     }
 
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
     public function edit($id)
     {
-        return "form para editar o produto de id: {$id}";
-    }
-    public function store()
-    {
-        return "cadastrando";
+        //
     }
 
-    public function update($id)
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
     {
-        return "Editando o produto de id: {$id}";
+        //
     }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
     public function destroy($id)
     {
-        return "Apagando o produto de id: {$id}";
+        //
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -2,13 +2,16 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::delete('products/{id}', 'ProductController@destroy')->name('products.destroy');
-Route::put('products/{id}', 'ProductController@update')->name('products.update');
-Route::get('products/{id}/edit', 'ProductController@edit')->name('products.edit');
-Route::get('products/create', 'ProductController@create')->name('products.create');
-Route::get('products/{id}', 'ProductController@show')->name('products.show');
-Route::get('products', 'ProductController@index')->name('products.index');
-Route::post('products', 'ProductController@store')->name('products.store');
+Route::resource('products', 'ProductController');//->middleware('auth');
+
+
+// Route::delete('products/{id}', 'ProductController@destroy')->name('products.destroy');
+// Route::put('products/{id}', 'ProductController@update')->name('products.update');
+// Route::get('products/{id}/edit', 'ProductController@edit')->name('products.edit');
+// Route::get('products/create', 'ProductController@create')->name('products.create');
+// Route::get('products/{id}', 'ProductController@show')->name('products.show');
+// Route::get('products', 'ProductController@index')->name('products.index');
+// Route::post('products', 'ProductController@store')->name('products.store');
 
 
 /*
